06a15190a561b229d29a44426d8cfc2f0149ae42
杨建成 <11967785+yang-jiancheng@user.noreply.gitee.com>
2026-02-01 18:36:01 +0800

    Initial commit

---
+++ src/main/java/com/yy/yy_ai_agent/YyAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yy.yy_ai_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yy/yy_ai_agent/YyAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yy.yy_ai_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
